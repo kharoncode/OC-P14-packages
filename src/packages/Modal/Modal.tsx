39a953c251b3c04d9a34440ea9b33388b9f6b4ee
Collin Rémi <sharon.code@proton.me>
2024-01-18 16:25:59 +0100

    Modal : add custom style

--- src/packages/Modal/Modal.tsx
+++ src/packages/Modal/Modal.tsx
@@ -1,15 +1,33 @@
+import styled from 'styled-components';
 import styles from './modal.module.css';
 
 type propsType = {
    open: boolean;
    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+   style?: string;
 };
 
+const Container = styled.div<{ display: string; $style: string }>`
+   display: ${({ display }) => display};
+   ${({ $style }) => $style}
+`;
+
+const defaultContainerStyle = `
+   position: absolute;
+   justify-content: center;
+   top: 0;
+   left: 0;
+   width: 100%;
+   backdrop-filter: blur(5px);
+   height: 100vh;
+   `;
+
 export const Modal = (props: propsType) => {
-   const { open, setOpen } = props;
-   //const display = open ? 'flex' : 'none';
+   const { open, setOpen, style } = props;
+   const display = open ? 'flex' : 'none';
+   const containerStyle = style ? style : defaultContainerStyle;
    return (
-      <div className={styles.container}>
+      <Container display={display} $style={containerStyle}>
          <div className={styles.modalContainer}>
             <button
                className={styles.closeButton}
@@ -17,8 +35,8 @@ export const Modal = (props: propsType) => {
             >
                Close
             </button>
-            MODAL
+            Employee Created!
          </div>
-      </div>
+      </Container>
    );
 };
