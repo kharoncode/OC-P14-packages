4a181d8edf711fef5491ac8549205af2a3407821
Collin Rémi <sharon.code@proton.me>
2024-01-19 11:54:00 +0100

    add Modal props

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # OC-P14-HRNet-Packages
+
+## Table of Contents
+
+-  [Installation](#installation)
+-  [modal](#modal)
+
+## Instalation
+
+To install, you can use [npm](https://npmjs.org/) or [yarn](https://yarnpkg.com):
+
+    $ npm install --save hrnet-packages
+    $ yarn add hrnet-packages
+
+## Modal
+
+### Props Type
+
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    children: string | JSX.Element;
+    background?: string;
+    modal?: string;
+    navigation?: { link: string; style?: string; text?: string };
--- src/packages/Modal/Modal.tsx
+++ src/packages/Modal/Modal.tsx
@@ -1,42 +1,99 @@
 import styled from 'styled-components';
 import styles from './modal.module.css';
+import closeIcone from '../../assets/icones/close.svg';
+import { useNavigate } from 'react-router-dom';
 
 type propsType = {
    open: boolean;
    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-   style?: string;
+   children: string | JSX.Element;
+   background?: string;
+   modal?: string;
+   navigation?: { link: string; style?: string; text?: string };
 };
 
 const Container = styled.div<{ display: string; $style: string }>`
-   display: ${({ display }) => display};
-   ${({ $style }) => $style}
-`;
-
-const defaultContainerStyle = `
    position: absolute;
    justify-content: center;
    top: 0;
    left: 0;
    width: 100%;
-   backdrop-filter: blur(5px);
    height: 100vh;
+   display: ${({ display }) => display};
+   ${({ $style }) => $style}
+`;
+
+const ModalContainer = styled.div<{ $style: string }>`
+   position: relative;
+   ${({ $style }) => $style}
+`;
+
+const NavButton = styled.div<{ $style: string }>`
+   ${({ $style }) => $style}
+`;
+
+const defaultContainerStyle = `
+   backdrop-filter: blur(5px);
+   `;
+
+const defaultModalStyle = `
+   display: flex;
+   flex-direction:column;
+   align-items: center;
+   gap:20px;
+   margin-top: 15vh;
+   padding: 30px;
+   width: 80%;
+   height: fit-content;
+   border: solid 5px black;
+   border-radius: 25px;
+   background-color: white;
+   color: black;
+   `;
+
+const defaultNavButtonStyle = `
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: #5955b3;
+color:white;
+padding:5px 10px 5px 10px;
+border-radius:10px;
+font-weight:500;
    `;
 
 export const Modal = (props: propsType) => {
-   const { open, setOpen, style } = props;
+   const navigate = useNavigate();
+   const { open, setOpen, children, background, modal, navigation } = props;
    const display = open ? 'flex' : 'none';
-   const containerStyle = style ? style : defaultContainerStyle;
+   const containerStyle = background ? background : defaultContainerStyle;
+   const modalStyle = modal ? modal : defaultModalStyle;
+   const navButtonStyle = navigation?.style
+      ? navigation.style
+      : defaultNavButtonStyle;
    return (
       <Container display={display} $style={containerStyle}>
-         <div className={styles.modalContainer}>
-            <button
+         <ModalContainer $style={modalStyle}>
+            <img
+               src={closeIcone}
+               alt="Close"
                className={styles.closeButton}
                onClick={() => setOpen(!open)}
-            >
-               Close
-            </button>
-            Employee Created!
-         </div>
+            />
+            {children}
+            {navigation ? (
+               <NavButton
+                  $style={navButtonStyle}
+                  onClick={() => {
+                     navigate(`/${navigation.link}/`);
+                  }}
+               >
+                  {navigation.text ? navigation.text : 'OK'}
+               </NavButton>
+            ) : (
+               <></>
+            )}
+         </ModalContainer>
       </Container>
    );
 };
