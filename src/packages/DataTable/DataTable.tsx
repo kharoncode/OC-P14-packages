0ce3233acde105261e55cdfc6a30fb8a3baa071e
Collin Rémi <sharon.code@proton.me>
2024-01-23 16:15:18 +0100

    show table with number of entry and page

--- src/packages/DataTable/DataTable.tsx
+++ src/packages/DataTable/DataTable.tsx
@@ -23,13 +23,19 @@ export const DataTable = (props: props) => {
    const columnsId = columns.map((el) => el.data);
    const [dataList, setDataList] = useState(Object.keys(data));
    const [isColumnSelected, setIsColumnSelected] = useState(columns[0].data);
+   const [tableLength, setTableLength] = useState(10);
+   const [page, setPage] = useState(1);
 
    return (
       <div className={styles.container}>
          <div className={styles.headerContainer}>
             <div className={styles.selectContainer}>
                <label htmlFor="showSelect">Show</label>
-               <select name="showSelect" id="showSelect">
+               <select
+                  name="showSelect"
+                  id="showSelect"
+                  onChange={(e) => setTableLength(Number(e.target.value))}
+               >
                   <option value="10">10</option>
                   <option value="25">25</option>
                   <option value="50">50</option>
@@ -60,30 +66,58 @@ export const DataTable = (props: props) => {
          </div>
          {dataList.length > 0 ? (
             <div className={styles.rowsContainer}>
-               {dataList.map((el: string) => {
-                  return (
-                     <div key={`${el}-row`} className={styles.row}>
-                        {columnsId.map((id) => {
-                           return (
-                              <div
-                                 key={`${el}-${id}`}
-                                 className={`${styles.item} ${id}`}
-                              >
-                                 {data[el][id]}
-                              </div>
-                           );
-                        })}
-                     </div>
-                  );
-               })}
+               {dataList
+                  .slice((page - 1) * tableLength, page * tableLength)
+                  .map((el: string) => {
+                     return (
+                        <div key={`${el}-row`} className={styles.row}>
+                           {columnsId.map((id) => {
+                              return (
+                                 <div
+                                    key={`${el}-${id}`}
+                                    className={`${styles.item} ${id}`}
+                                 >
+                                    {data[el][id]}
+                                 </div>
+                              );
+                           })}
+                        </div>
+                     );
+                  })}
             </div>
          ) : (
             <div className={styles.noData}>No data available in table</div>
          )}
 
          <div className={styles.info}>
-            <div>Showing {Object.keys(data).length} entries</div>
-            <div>Prev / Next</div>
+            <div>
+               Showing{' '}
+               {dataList.length === 0
+                  ? 0
+                  : page * tableLength - (tableLength - 1)}{' '}
+               to{' '}
+               {page * tableLength > dataList.length
+                  ? dataList.length
+                  : page * tableLength}{' '}
+               of {Object.keys(data).length} entries
+            </div>
+            {dataList.length > tableLength ? (
+               <div className={styles.pagesList}>
+                  Prev
+                  {[...Array(Math.ceil(dataList.length / tableLength))].map(
+                     (e, i) => {
+                        return (
+                           <button key={i} onClick={() => setPage(i + 1)}>
+                              {i + 1}
+                           </button>
+                        );
+                     }
+                  )}
+                  Next
+               </div>
+            ) : (
+               <></>
+            )}
          </div>
       </div>
    );
