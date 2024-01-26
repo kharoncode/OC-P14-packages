b856b192d3cda0243fd8368f52d9d2a0c7c5c2f6
Collin Rémi <sharon.code@proton.me>
2024-01-26 12:45:16 +0100

    create useStyle hook

--- src/packages/utils/useStyle.ts
+++ src/packages/utils/useStyle.ts
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 
 export const UseStyle = (
-   style: { [key: string]: string } | undefined,
+   style: CSSModuleClasses | undefined,
    styles: CSSModuleClasses,
    className: CSSModuleClasses,
    setClassName: React.Dispatch<React.SetStateAction<{ [key: string]: string }>>
 ) => {
-   console.log('useStyle');
    useEffect(() => {
       if (style) {
+         console.log('useStyle');
          const newStyle: { [key: string]: string } = {};
          Object.keys(className).map((key) => {
             newStyle[key] = style[key] ? style[key] : styles[key];
